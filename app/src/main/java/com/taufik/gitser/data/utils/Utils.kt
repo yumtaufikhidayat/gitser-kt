280fe53011e2b37736af6fc80e190baed22830d5
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-08 15:15:53 +0700

    Changing token of app.

--- app/src/main/java/com/taufik/gitser/data/utils/Utils.kt
+++ app/src/main/java/com/taufik/gitser/data/utils/Utils.kt
@@ -2,5 +2,5 @@ package com.taufik.gitser.data.utils
 
 object Utils {
     const val BASE_URL = "https://api.github.com/"
-    const val TOKEN_AUTH = "fe6d2cbbe309ec6af61d02cc1991f21ff22dedb1"
+    const val TOKEN_AUTH = "76bcefd11dadb523b91dd6cb4545025f0a27fccd"
 }
