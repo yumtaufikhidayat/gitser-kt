ed8e55f43a7d6dc7eecee1ba3c5f6ab727b1894d
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-08 15:54:15 +0700

    Updating project.

--- app/src/main/java/com/taufik/gitser/data/utils/Utils.kt
+++ app/src/main/java/com/taufik/gitser/data/utils/Utils.kt
@@ -3,5 +3,5 @@ package com.taufik.gitser.data.utils
 object Utils {
     const val BASE_URL = "https://api.github.com/"
     const val TOKEN_AUTH = "91b51da64ff2b19d8afe78606fd6d30997448bdf"
-    const val SEARCH = "search/users"
+    const val SEARCH_USERS = "search/users"
 }
