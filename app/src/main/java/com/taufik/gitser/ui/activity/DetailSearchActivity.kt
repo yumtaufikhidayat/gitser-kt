cb9944de8acd3e8cf760b52a0ff9db29925357b3
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-10 06:51:29 +0700

    Adding detail search activity.

---
+++ app/src/main/java/com/taufik/gitser/ui/activity/DetailSearchActivity.kt
@@ -0,0 +1,17 @@
+package com.taufik.gitser.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.taufik.gitser.databinding.ActivityDetailSearchBinding
+
+class DetailSearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailSearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailSearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
