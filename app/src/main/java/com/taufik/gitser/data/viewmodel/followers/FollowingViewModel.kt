129eb4acb173e512f4809986234dec77d5f032a9
yumtaufikhidayat <yumtaufik1997@gmail.com>
2021-02-10 14:52:43 +0700

    Adding followers fragment.

---
+++ app/src/main/java/com/taufik/gitser/data/viewmodel/followers/FollowingViewModel.kt
@@ -0,0 +1,40 @@
+package com.taufik.gitser.data.viewmodel.followers
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.taufik.gitser.api.ApiClient
+import com.taufik.gitser.data.model.search.Search
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class FollowingViewModel : ViewModel() {
+
+    val listOfFollowing = MutableLiveData<ArrayList<Search>>()
+
+    fun setListOfFollowing(username: String) {
+        ApiClient.apiInstance
+            .getFollowingUsers(username)
+            .enqueue(object : Callback<ArrayList<Search>> {
+                override fun onResponse(
+                    call: Call<ArrayList<Search>>,
+                    response: Response<ArrayList<Search>>
+                ) {
+                    if (response.isSuccessful) {
+                        listOfFollowing.postValue(response.body())
+                    }
+                    Log.e("followingSuccess", "onResponse: ${response.body()}")
+                }
+
+                override fun onFailure(call: Call<ArrayList<Search>>, t: Throwable) {
+                    Log.e("followingFailed", "onFailure: ${t.localizedMessage}")
+                }
+            })
+    }
+
+    fun getListOfFollowing(): LiveData<ArrayList<Search>> {
+        return listOfFollowing
+    }
+}
--- app/src/main/java/com/taufik/gitser/ui/fragment/following/FollowingFragment.kt
+++ app/src/main/java/com/taufik/gitser/ui/fragment/following/FollowingFragment.kt
@@ -3,17 +3,82 @@ package com.taufik.gitser.ui.fragment.following
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.taufik.gitser.R
+import com.taufik.gitser.adapter.SearchAdapter
+import com.taufik.gitser.data.viewmodel.followers.FollowingViewModel
 import com.taufik.gitser.databinding.FragmentFollowsBinding
+import com.taufik.gitser.ui.activity.detail.DetailSearchActivity
 
 class FollowingFragment : Fragment(R.layout.fragment_follows) {
 
     private var _binding: FragmentFollowsBinding? = null
     private val binding get() = _binding!!
 
+    private lateinit var viewModel: FollowingViewModel
+    private lateinit var adapter: SearchAdapter
+    private lateinit var username: String
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setArgument()
+
         _binding = FragmentFollowsBinding.bind(view)
-        
+
+        setAdapter()
+
+        setViewModel()
+    }
+
+    private fun setArgument() {
+
+        val argument = arguments
+        username = argument?.getString(DetailSearchActivity.EXTRA_USERNAME).toString()
+    }
+
+    private fun setAdapter() {
+
+        adapter = SearchAdapter()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun setViewModel() {
+
+        binding.apply {
+            rvFollows.setHasFixedSize(true)
+            rvFollows.layoutManager = LinearLayoutManager(requireActivity())
+            rvFollows.adapter = adapter
+        }
+
+        showLoading(true)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.NewInstanceFactory())
+            .get(FollowingViewModel::class.java)
+
+        viewModel.setListOfFollowing(username)
+        viewModel.getListOfFollowing().observe(viewLifecycleOwner, {
+            if (it != null) {
+                adapter.setSearchUserList(it)
+                showLoading(false)
+            }
+        })
+    }
+
+    private fun showLoading(state: Boolean) {
+
+        if (state) {
+            binding.progressBarFollows.visibility = View.VISIBLE
+        } else {
+            binding.progressBarFollows.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
