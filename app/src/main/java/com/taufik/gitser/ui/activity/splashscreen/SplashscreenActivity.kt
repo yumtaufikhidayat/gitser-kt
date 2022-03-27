83104dfc76f6df5b07fb970cf0848181292c0a80
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-27 11:39:15 +0700

    Change app version text.

--- app/src/main/java/com/taufik/gitser/ui/activity/splashscreen/SplashscreenActivity.kt
+++ app/src/main/java/com/taufik/gitser/ui/activity/splashscreen/SplashscreenActivity.kt
@@ -39,7 +39,7 @@ class SplashscreenActivity : AppCompatActivity() {
             try {
                 val pInfo: PackageInfo = packageManager.getPackageInfo(packageName, 0)
                 val appVersion = pInfo.versionName
-                tvAppVersion.text = String.format("Versi %s", appVersion)
+                tvAppVersion.text = appVersion
             } catch (e: PackageManager.NameNotFoundException) {
                 e.printStackTrace()
             }
