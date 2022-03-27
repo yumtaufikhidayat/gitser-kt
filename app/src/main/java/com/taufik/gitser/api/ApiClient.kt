17b9dfd0a0c23d1684140d8a706ff3874c510e0a
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-27 08:52:51 +0700

    Add interceptor and okHttpClient for network debugging.

--- app/src/main/java/com/taufik/gitser/api/ApiClient.kt
+++ app/src/main/java/com/taufik/gitser/api/ApiClient.kt
@@ -1,14 +1,29 @@
 package com.taufik.gitser.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 object ApiClient {
+    private const val timeOutTime = 30L
+
+    private val interceptor = HttpLoggingInterceptor()
+        .setLevel(HttpLoggingInterceptor.Level.BODY)
+
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(interceptor)
+        .connectTimeout(timeOutTime, TimeUnit.SECONDS)
+        .writeTimeout(30, TimeUnit.SECONDS)
+        .readTimeout(30, TimeUnit.SECONDS)
+        .build()
 
     private val retrofit: Retrofit = Retrofit.Builder()
-            .baseUrl(UrlEndpoint.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+        .baseUrl(UrlEndpoint.BASE_URL)
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
 
     val apiInstance: Api = retrofit.create(Api::class.java)
 }
