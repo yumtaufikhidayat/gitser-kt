0f363dc442da2d5a741f5c74495992d9f4ea3145
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-03-27 09:38:01 +0700

    Update extension function for imageview.

--- app/src/main/java/com/taufik/gitser/utils/Utils.kt
+++ app/src/main/java/com/taufik/gitser/utils/Utils.kt
@@ -4,37 +4,46 @@ import android.text.*
 import android.text.method.LinkMovementMethod
 import android.text.style.ClickableSpan
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 
-class Utils {
-    companion object {
-        fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>){
-            val spannableString = SpannableString(this.text)
-            var startIndexOfLink = -1
+object Utils {
+    fun TextView.makeLinks(vararg links: Pair<String, View.OnClickListener>){
+        val spannableString = SpannableString(this.text)
+        var startIndexOfLink = -1
 
-            for (link in links) {
-                val clickableSpan = object : ClickableSpan(){
+        for (link in links) {
+            val clickableSpan = object : ClickableSpan(){
 
-                    override fun updateDrawState(ds: TextPaint) {
-                        ds.color = ds.linkColor
-                        ds.isUnderlineText = false
-                    }
-
-                    override fun onClick(view: View) {
-                        Selection.setSelection((view as TextView).text as Spannable, 0)
-                        view.invalidate()
-                        link.second.onClick(view)
-                    }
+                override fun updateDrawState(ds: TextPaint) {
+                    ds.color = ds.linkColor
+                    ds.isUnderlineText = false
                 }
 
-                startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
-                spannableString.setSpan(
-                        clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
-                )
+                override fun onClick(view: View) {
+                    Selection.setSelection((view as TextView).text as Spannable, 0)
+                    view.invalidate()
+                    link.second.onClick(view)
+                }
             }
 
-            this.movementMethod = LinkMovementMethod.getInstance()
-            this.setText(spannableString, TextView.BufferType.SPANNABLE)
+            startIndexOfLink = this.text.toString().indexOf(link.first, startIndexOfLink + 1)
+            spannableString.setSpan(
+                    clickableSpan, startIndexOfLink, startIndexOfLink + link.first.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+            )
         }
+
+        this.movementMethod = LinkMovementMethod.getInstance()
+        this.setText(spannableString, TextView.BufferType.SPANNABLE)
+    }
+
+    fun ImageView.loadImage(url: String?) {
+        Glide.with(this.context)
+            .load(url)
+            .apply(RequestOptions().override(500, 500))
+            .centerCrop()
+            .into(this)
     }
 }
