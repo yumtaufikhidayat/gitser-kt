16e9e694d3a2a7009d4057a55e3bf74fce30ce66
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-04-17 06:35:44 +0700

    Upgrade database version.

--- app/src/main/java/com/taufik/gitser/data/local/UserDatabase.kt
+++ app/src/main/java/com/taufik/gitser/data/local/UserDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [FavoriteEntity::class], version = 2)
+@Database(entities = [FavoriteEntity::class], version = 3)
 abstract class UserDatabase : RoomDatabase(){
 
     companion object{
