68bd9bbfbdfbdb5363b77ebf0ad990570bdf0bd1
yumtaufikhidayat <yumtaufik1997@gmail.com>
2022-05-30 07:39:10 +0700

    Update adapter using list adapter and diff util.

--- app/src/main/java/com/taufik/gitser/adapter/repository/RepositoryAdapter.kt
+++ app/src/main/java/com/taufik/gitser/adapter/repository/RepositoryAdapter.kt
@@ -5,67 +5,69 @@ import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.taufik.gitser.data.response.detail.RepositoryResponse
 import com.taufik.gitser.databinding.ItemRepositoryBinding
 import kotlin.math.roundToInt
 
-class RepositoryAdapter : RecyclerView.Adapter<RepositoryAdapter.MyViewHolder>(){
+class RepositoryAdapter : ListAdapter<RepositoryResponse, RepositoryAdapter.RepositoryViewHolder>(RepositoryDiffCallback){
 
-    private val listRepository = ArrayList<RepositoryResponse>()
-
-    fun setRepositoryList(repos: ArrayList<RepositoryResponse>) {
-        listRepository.clear()
-        listRepository.addAll(repos)
-        notifyDataSetChanged()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RepositoryViewHolder {
+        return RepositoryViewHolder(ItemRepositoryBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    inner class MyViewHolder(private val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root) {
+    override fun onBindViewHolder(
+        holder: RepositoryViewHolder,
+        position: Int
+    ) = holder.bind(getItem(position))
 
-        fun bind(repository: RepositoryResponse) {
+    inner class RepositoryViewHolder(private val binding: ItemRepositoryBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(repository: RepositoryResponse) = with(binding){
+            tvRepositoryName.text = repository.name
+            tvRepositoryDesc.text = repository.description
+            tvRepositorySize.text = repository.size.toString()
+            tvRepositoryLanguage.text = repository.language
 
-            binding.apply {
-                tvRepositoryName.text = repository.name
-                tvRepositoryDesc.text = repository.description
-                tvRepositorySize.text = repository.size.toString()
-                tvRepositoryLanguage.text = repository.language
+            val repoSize = repository.size
+            tvRepositorySize.text = repoSize.toString()
 
-                val repoSize = repository.size
-                tvRepositorySize.text = repoSize.toString()
+            val sizeStr = tvRepositorySize.text.toString().trim()
+            val sizeInt = sizeStr.toInt()
+            val sizeDouble = sizeInt / 1000.toFloat()
+            val sizeIntNew = sizeDouble.roundToInt()
 
-                val sizeStr = tvRepositorySize.text.toString().trim()
-                val sizeInt = sizeStr.toInt()
-                val sizeDouble = sizeInt / 1000.toFloat()
-                val sizeIntNew = sizeDouble.roundToInt()
+            if (sizeIntNew < 1) {
+                val sizeDoubleKb = (sizeDouble * 1000).roundToInt()
+                val sizeIntKb = sizeDoubleKb.toDouble().roundToInt()
+                tvRepositorySize.text = String.format("%s KB", sizeIntKb)
+            } else {
+                tvRepositorySize.text = String.format("%s MB", sizeIntNew)
+            }
 
-                if (sizeIntNew < 1) {
-                    val sizeDoubleKb = (sizeDouble * 1000).roundToInt()
-                    val sizeIntKb = sizeDoubleKb.toDouble().roundToInt()
-                    tvRepositorySize.text = String.format("%s KB", sizeIntKb)
-                } else {
-                    tvRepositorySize.text = String.format("%s MB", sizeIntNew)
+            itemView.setOnClickListener {
+                try {
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(repository.html_url))
+                    itemView.context.startActivity(Intent.createChooser(intent, "Open with:"))
+                } catch (e: Exception) {
+                    Log.e("errorIntent", "onBindViewHolder: ${e.localizedMessage}")
                 }
             }
         }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = ItemRepositoryBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(view)
-    }
+    companion object {
+        object RepositoryDiffCallback : DiffUtil.ItemCallback<RepositoryResponse>() {
+            override fun areItemsTheSame(
+                oldItem: RepositoryResponse,
+                newItem: RepositoryResponse
+            ): Boolean = oldItem.name == newItem.name
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val pos = listRepository[position]
-        holder.bind(pos)
-        holder.itemView.setOnClickListener{
-            try {
-                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(pos.html_url))
-                holder.itemView.context.startActivity(Intent.createChooser(intent, "Open with:"))
-            } catch (e: Exception) {
-                Log.e("errorIntent", "onBindViewHolder: ${e.localizedMessage}")
-            }
+            override fun areContentsTheSame(
+                oldItem: RepositoryResponse,
+                newItem: RepositoryResponse
+            ): Boolean = oldItem == newItem
         }
     }
-
-    override fun getItemCount(): Int = listRepository.size
 }
--- app/src/main/java/com/taufik/gitser/adapter/search/SearchAdapter.kt
+++ app/src/main/java/com/taufik/gitser/adapter/search/SearchAdapter.kt
@@ -3,48 +3,45 @@ package com.taufik.gitser.adapter.search
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.taufik.gitser.data.response.search.Search
 import com.taufik.gitser.databinding.ItemUserBinding
 import com.taufik.gitser.ui.activity.detail.DetailSearchActivity
 import com.taufik.gitser.utils.Utils.loadImage
 
-class SearchAdapter : RecyclerView.Adapter<SearchAdapter.MyViewHolder>() {
+class SearchAdapter: ListAdapter<Search, SearchAdapter.SearchViewHolder>(SearchDiffCallback) {
 
     private val listUsers = ArrayList<Search>()
 
-    fun setSearchUserList(searches: ArrayList<Search>) {
-        listUsers.clear()
-        listUsers.addAll(searches)
-        notifyDataSetChanged()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
+        return SearchViewHolder(ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 
-    inner class MyViewHolder(private val binding: ItemUserBinding)
-        : RecyclerView.ViewHolder(binding.root) {
-        fun bind(search: Search) {
-            binding.apply {
-                imgUserProfile.loadImage(search.avatarUrl)
-                tvUsernameProfile.text = search.login
-                tvProfileType.text = search.type
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = ItemUserBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return MyViewHolder(view)
-    }
-
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val pos = listUsers[position]
-        holder.bind(pos)
-        holder.itemView.setOnClickListener {
-            val intent = Intent(holder.itemView.context, DetailSearchActivity::class.java).apply {
-                putExtra(DetailSearchActivity.EXTRA_DATA, pos)
-            }
-            it.context.startActivity(intent)
-        }
-    }
+    override fun onBindViewHolder(holder: SearchViewHolder, position: Int) = holder.bind(getItem(position))
 
     override fun getItemCount(): Int = listUsers.size
+
+    inner class SearchViewHolder(private val binding: ItemUserBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(search: Search) = with(binding) {
+            imgUserProfile.loadImage(search.avatarUrl)
+            tvUsernameProfile.text = search.login
+            tvProfileType.text = search.type
+
+            itemView.setOnClickListener {
+                val intent = Intent(itemView.context, DetailSearchActivity::class.java).apply {
+                    putExtra(DetailSearchActivity.EXTRA_DATA, search)
+                }
+                it.context.startActivity(intent)
+            }
+        }
+    }
+
+    companion object {
+        object SearchDiffCallback : DiffUtil.ItemCallback<Search>() {
+            override fun areItemsTheSame(oldItem: Search, newItem: Search): Boolean = oldItem.id == newItem.id
+            override fun areContentsTheSame(oldItem: Search, newItem: Search): Boolean = oldItem == newItem
+        }
+    }
 }
